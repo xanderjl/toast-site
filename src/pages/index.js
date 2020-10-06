fa4ed2df1dc6c7ba499f8d6f5d9ad1f8c6b8e312
XanderJL <alexanderjameslow@gmail.com>
2020-10-06 01:04:43 -0400

    first.5 commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import { h } from "preact";
+
+const Index = () => {
+  return (
+    <body>
+      <div className="header">
+        <h1>Hello, Toast.</h1>
+      </div>
+    </body>
+  );
+};
+
+export default Index;
